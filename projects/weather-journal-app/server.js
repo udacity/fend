49fd589bf43019609c04406bfeef61835ea07d0b
Natalie Richards <nrichards.sb.31@gmail.com>
2019-12-30 13:09:16 -0800

    Pushed changes

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,74 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
+
+const bodyParser = require('body-parser');
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+
+// Spin up the server
+const server = app.listen(port, listening)
+
+function listening(){
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
+
+
+//GET route
+
+app.get('/getProjectData', function (req, res) {
+
+    res.send(projectData);
+  })
+
+
+
+const weatherData = [];
+
+//get request is off timing. returning blank on first call while post returns first entry
+  app.get('/all', getData) 
+  function getData (req, res){
+    res.send(weatherData);
+    console.log(weatherData);
+  }
+
+
+  //post route adds entry - worls
+
+app.post('/add', addEntry);
+
+  function addEntry(req,res){
+//   console.log(req.body);
+    newEntry = {
+      temp: req.body.temp,
+      desc: req.body.desc,
+      city: req.body.city,
+      feelings: req.body.feelings,
+      date: req.body.date,
+      time: req.body.time
+    }
+  
+    weatherData.push(newEntry);
+    res.send(weatherData)
+    // console.log(weatherData);
+  }
+
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -2,4 +2,141 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = (d.getMonth() + 1) +'/'+ d.getDate()+'/'+ d.getFullYear();
+let newTime = d.getHours()+':'+ d.getMinutes();
+
+
+
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+
+let apiKey = '&APPID=bbee3673552ca695c2b8fa655569a66d';
+
+
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+    function performAction(e){
+        const newZip =  document.getElementById('zip').value;
+        let zip = newZip + ',us';
+
+        const feeling = document.getElementById('feelings').value;
+
+        getEntry(baseURL, zip, apiKey)
+
+        .then(function(data){
+
+            postData('/add', {  temp: data.main.temp, 
+                                desc: data.weather[0].description,
+                                city: data.name,
+                                feelings: feeling,
+                                date: newDate,
+                                time: newTime
+
+                            }                                
+                    );
+          })
+          .then(
+            setTimeout(function(){
+            updateUI()
+    }, 2000)
+          )
+          
+    }
+
+//this works
+const getEntry = async (baseURL, zip, key)=>{
+
+    const res = await fetch(baseURL+zip+key)
+    try {
+  
+      const weatherInfo = await res.json();
+      console.log(weatherInfo)
+        
+      return weatherInfo;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+
+};
+
+
+
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+  try {
+    const newData = await response.json();
+    return newData;
+  }catch(error) {
+  console.log("error", error)
+  }
+}
+
+
+
+//get request is off timing. returning blank on first call while post returns first entry
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+      const allData = await request.json();
+      console.log(allData);
+    
+
+
+      for (let i = 0; i < allData.length; i++) {
+    //     let end = allData.length;
+ 
+    //     if (i === end) {
+    //     //display date
+    //         document.getElementById('date').innerHTML = 'On ' + revData[0].date + ' at ' + revData[0].time;
+
+    //         document.getElementById('city').innerHTML = 'in ' + revData[0].city + ',';
+
+    //         //convert and display temp
+    //         const farTemp = (revData[0].temp - 273.15) * 9/5 + 32;
+    //         const roundTemp = Math.round(farTemp);
+    //         document.getElementById('temp').innerHTML = 'It was ' + roundTemp + '&deg with ';
+
+    //         //display description
+    //         document.getElementById('desc').innerHTML = revData[0].desc;
+
+    //         document.getElementById('content').innerHTML = revData[0].feelings;
+    //     }
+    //   else{
+       
+            let oldEntry = document.createElement('div');
+            let oldTitle = document.createElement('h3');
+            let oldContent = document.createElement('p');
+
+            oldEntry.setAttribute('class', 'prev-entry');
+
+            oldTitle.setAttribute('class', 'prev-title');
+
+            let farTemp = (allData[i].temp - 273.15) * 9/5 + 32;
+            let roundTemp = Math.round(farTemp);
+            oldTitle.innerHTML = allData[i].date + ' at ' + allData[i].time + ' -> It was ' + roundTemp + '&deg in ' + allData[i].city + ', with ' + allData[i].desc;
+
+            document.getElementById('entry-logs').append(oldEntry);
+            document.getElementById('entry-logs').append(oldTitle);
+         
+            oldContent.setAttribute('class', 'prev-content');
+            oldContent.innerHTML = '<p>' + allData[i].feelings + '</p>';
+
+            oldEntry.append(oldContent);
+
+        }
+    //  }
+        return allData;
+    }catch(error){
+      console.log("error", error);
+    }
+  }
+  
