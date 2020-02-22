7a254dff228fb55d3efd3316af31dba23e3dc921
jeraquis <jeraquis@users.noreply.github.com>
2020-02-21 21:30:15 -0800

    first pass

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,6 +17,9 @@
  * Define Global Variables
  * 
 */
+const sections = document.querySelectorAll('section');
+const navList = document.querySelector('#navbar__list');
+const upButton = document.querySelector('#up-button');
 
 
 /**
@@ -25,6 +28,13 @@
  * 
 */
 
+function buttonAppear() {
+    if (document.documentElement.scrollTop > 500) {
+        upButton.style.display = 'block';
+    } else {
+        upButton.style.display = 'none';
+    }
+}
 
 
 /**
@@ -35,23 +45,70 @@
 
 // build the nav
 
+function buildNav() {
+    for (const section of sections) {
+        const sectionName = section.dataset.nav;
+        const sectionNum = section.id;
+        const navHtml = `<li><a data-target="${sectionNum}" id="click" class="menu__link"> ${sectionName} </li>`;
+        navList.insertAdjacentHTML('beforeend', navHtml);
+    }
+}
 
 // Add class 'active' to section when near top of viewport
-
+function makeActive() {
+    const activeSection = document.querySelector('.your-active-class');
+    // first 'if' in case active class has already been removed
+    if (activeSection != null) {
+        const activeLocation = activeSection.getBoundingClientRect();
+        if (activeLocation.bottom < (window.innerHeight / 2)) {
+            activeSection.classList.remove('your-active-class');
+        }
+    }
+    for (let i=0; i<sections.length; i++) {
+        let sect = document.querySelector(`#section${i+1}`);
+        const sectionLoc = sect.getBoundingClientRect();
+        if ((sectionLoc.top < (window.innerHeight / 2))
+            && (sectionLoc.bottom > (window.innerHeight / 2))) {
+            sect.classList.add('your-active-class');
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
+function scrollTO(evt) {
+    const targetId = evt.target.dataset.target;
+    const targetSection = document.querySelector(`#${targetId}`);
+    targetSection.scrollIntoView({
+        behavior: 'smooth'
+    });
+}    
 
+function goToTop() {
+    const scroll = document.scrollingElement;
+    scroll.scrollTop = 0;
+}
 
-/**
+/*
  * End Main Functions
  * Begin Events
  * 
 */
 
 // Build menu 
+buildNav();
 
 // Scroll to section on link click
+navList.addEventListener('click', event => {
+    scrollTO(event);
+});
 
 // Set sections as active
+window.addEventListener('scroll', makeActive);
+
+
+// Scroll to top
+upButton.addEventListener('click', goToTop);
+
+window.addEventListener('scroll', buttonAppear);
 
 
