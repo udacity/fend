a7353974b37abe58c72052bebd20b55258bbde04
jeraquis <jeraquis@users.noreply.github.com>
2020-03-16 12:57:35 -0700

    commit-to-switch
    have to commit to switch commits

--- projects/evaluate-news-nlp/src/client/js/getAPI.js
+++ projects/evaluate-news-nlp/src/client/js/getAPI.js
@@ -4,6 +4,30 @@ function getInfo(input) {
     console.log(input)
     const request = baseURL + input
     console.log(request)
+
+    const getting = async (url = '', data = {}) => {
+        console.log('in getting')
+        const receive = await fetch(request, {
+            method: 'GET',
+            mode: 'no-cors',
+            credentials: 'include',
+            headers: {
+                'X-AYLIEN-TextAPI-Application-Key': '35d5f8bfd2d18d0ac19c63195968b59e',
+                'X-AYLIEN-TextAPI-Application-ID': '607aef20',
+                'Content-Type': 'application/json',
+            },
+            })
+        try {
+            const APIdata = await receive.json()
+            console.log(APIdata)
+            return APIdata
+        } catch(error) {
+            console.log('error', error)
+        }
+    }
+    const boop = getting()
+    console.log(boop)
+/*
     var aylien = require("aylien_textapi")
     var textapi = new aylien({
         application_id: process.env.API_ID,
@@ -17,6 +41,7 @@ function getInfo(input) {
         }
     })
     console.log(test)
+    */
 }
 
 /* API SDK
