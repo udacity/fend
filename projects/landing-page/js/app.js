a5cfd2d7f56d179c8ad152a04a54e5bbb2538628
huy <huynguyen120390@gmail.com>
2020-04-07 21:31:15 -0700

    Add finished weather app journal

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,45 +18,26 @@
  * 
 */
 const ACTIVE_TOP_COORD_PERCENT = 0.2;
-
-
+const SECTION_FIT_OFFSET  = 150;
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-function build_the_nav(){
-    const navbar = document.createDocumentFragment();
-    const sectionCount = document.querySelectorAll('section').length;
-    for(let i = 0; i < sectionCount; i++){
-        const list_item = document.createElement('li');
-        const list_link = document.createElement('a');
-        list_link.href = `#section${i+1}`;
-        list_link.innerHTML = `Section ${i+1}`;
-        list_link.style.color = '#fff';
-        list_item.appendChild(list_link);
-        navbar.appendChild(list_item);
+//function helps scroll to section
+function scroll_to_section(evt){
+    //if click on A or LI 
+    if(evt.target.nodeName == 'A' || evt.target.nodeName == 'LI'){
+        const targeted_section_text = evt.target.textContent;
+        const section_number = Number(targeted_section_text.split(" ")[1]);
+        let sections = document.querySelectorAll('section');
+        let targeted_section = sections[section_number - 1];
+        let section_y = targeted_section.offsetTop;
+        window.scrollTo({left:0,top:section_y - SECTION_FIT_OFFSET,behavior:'smooth'});
     }
-    document.querySelector('#navbar__list').appendChild(navbar);
 }
 
-// Add class 'active' to section when near top of viewport
-function add_mousewheel_listener_to_sections(){
-    const section = document.querySelector('section');
-    section.parentElement.addEventListener('mousewheel',add_active_class);
-    window.addEventListener('scroll',add_active_class);
-}
-
+//function help switch active class from previous active class to current active class 
 function add_active_class(){
     //detect section
     const sections = document.querySelectorAll('section');
@@ -72,6 +53,7 @@ function add_active_class(){
     for(let i = 0; i <sections.length; i++){
         let y = sections[i].getBoundingClientRect().y;
         if(y< ACTIVE_TOP_COORD_PERCENT*window.innerHeight && y >0){
+            //console.log(sections[i].id , sections[i].getBoundingClientRect().y);
             cur_active_section_index = i;
             break;
         }
@@ -86,22 +68,52 @@ function add_active_class(){
     //add active to current section
     sections[cur_active_section_index].classList.add('your-active-class');
 }
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+// build the nav
+function build_the_nav(){
+    const navbar = document.createDocumentFragment();
+    const sectionCount = document.querySelectorAll('section').length;
+    for(let i = 0; i < sectionCount; i++){
+        const list_item = document.createElement('li');
+        const list_link = document.createElement('a');
+        list_link.innerHTML = `Section ${i+1}`;
+        list_link.style.color = '#fff';
+        list_item.appendChild(list_link);
+        navbar.appendChild(list_item);
+    }
+    document.querySelector('#navbar__list').appendChild(navbar);
+}
 
-build_the_nav();
-//add_active_class();
-add_mousewheel_listener_to_sections();
+// Add class 'active' to section when near top of viewport
+function add_mousewheel_listener_to_sections(){
+    const section = document.querySelector('section');
+    section.parentElement.addEventListener('mousewheel',add_active_class);
+    window.addEventListener('scroll',add_active_class);
 
+}
 
+function add_click_to_scroll_listener_to_sections(){
+    const nav_list = document.querySelector('#navbar__list');
+    nav_list.addEventListener('click',function(evt){scroll_to_section(evt);});
+}
+
+// Scroll to anchor ID using scrollTO event
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
 // Build menu 
-
+build_the_nav();
 // Scroll to section on link click
-
+add_mousewheel_listener_to_sections();
 // Set sections as active
+add_click_to_scroll_listener_to_sections();
+
+
 
 
--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8006;
+app.listen(port,()=>{console.log(`server listening to port ${port}`)});
+
+let dataCount = -1;
+//GET route 
+app.get('/all',(req,res)=>{
+    res.send(projectData);
+});
+
+//POST route
+app.post('/addWeather',(req,res)=>{
+    newEntry = {
+        temp: req.body.temp,
+        content: req.body.content,
+        feeling: req.body.feeling,
+    }
+    dataCount += 1
+    projectData[dataCount] = newEntry;
+    //res.send(projectData);
+    console.log("Server received weather data",projectData);
+});
+
