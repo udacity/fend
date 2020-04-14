9ca3e72066840b8aa71b551ec34ab569f5ea33f1
huy <huynguyen120390@gmail.com>
2020-04-13 19:50:10 -0700

    Add new

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,18 +1,39 @@
+var path = require('path');
 const dotenv = require('dotenv');
-dotenv.config();
+const result = dotenv.config({encoding:'utf8'});
+
+
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+console.log('result',result);
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(express.static('dist'))
 var aylien = require("aylien_textapi");
 var textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
     });
+console.log(textapi);
+console.log('application id',textapi._options.application_id);
+console.log('application key',textapi._options.application_key);
 
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-
-const app = express()
-
-app.use(express.static('dist'))
+textapi.entityLevelSentiment({'url': '=http://techcrunch.com/2015/07/16/microsoft-will-never-give-up-on-mobile'}, function(error, response) {
+    if (error === null) {
+        console.log(response);
+    }else{
+        console.log("error",error);
+        console.log("response",response);
+    }
+    });
 
 console.log(__dirname)
 
@@ -29,3 +50,5 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
