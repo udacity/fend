1b196d2809592e5e27375dd7285119abe0789bba
koby-kramer <koby.e.kramer@gmail.com>
2020-09-07 14:29:27 -0700

    Added graded blog and gradend landing-page projects.

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -1,13 +1,10 @@
-# Landing Page Project
+# FEND Landing Page Project
+In this project, we're being asked to complete three different things but do it dynamically with Javascript.
+1. Add a navigation Bar.
+2. Monitor the Active Section of the HTML page. 
+3. Scroll seemlessly to the section when clicked from Navbar. 
 
-## Table of Contents
+## How to do this?
+To do this we're going to have to split the HTML into sections using an array, so we can manipulate that array. By having an array we can just call the sections from that and keep an active tracking. We're also going to need to have a place hodler for active section and a remove class function.
 
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+A user can move through the page 1 of 2 ways, by scrolling or click and what we'd like to do, is once a person clicks from the navbar, we'd like to replcace the active section with the section theyb just clicked. However, if someone is just scrolling throught he page, the middle section, or one most present to them will be the main section that will hold our active place holder. As we add more sections, they will be added to the navbar from the javascript we built to dynamically build the navbar, rather than relying on the HTML to do it. 
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,41 +17,117 @@
  * Define Global Variables
  * 
 */
+(function (global) {
+    const pageSections = Array.from(
+        document.querySelectorAll("section[data-nav]")
+    );
+    const navItemsContainer = document.querySelector("#navbar__list");
+    const navAnchors = navItemsContainer.getElementsByTagName("a");
+    let clickActive = false;
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+    /**
+     * End Global Variables
+     * Start Helper Functions
+     * 
+    */
+
+    const removeClass = (klass, elems) => {
+        Array.from(elems).forEach(elem => elem.classList.remove(klass))
+
+    };
+
+    const handleScroll = () => {
+        if (clickActive) return;
+
+        const activePageSection = pageSections.find(section => {
+            const lowerRange = section.offsetTop;
+            const upperRange = section.offsetTop + section.offsetHeight;
+            const scrollPos = global.pageYOffset + 250;
+
+            return scrollPos >= lowerRange && scrollPos <= upperRange;
+        });
+        let activeNavAnchor;
+
+        removeClass("active-section", pageSections);
+        removeClass("active", navAnchors);
+
+        if (activePageSection) {
+            activeNavAnchor = Array.from(navAnchors).find(
+                anchor => anchor.getAttribute("href").slice(1) === activePageSection.id
+            );
+
+            activePageSection.classList.add("active-section");
+            activeNavAnchor.classList.add("active");
+        }
+    };
+
+    const handleClick = e => {
+        e.preventDefault();
+        clickActive = true;
+        const targetElem = e.target;
+
+        if (targetElem.nodeName === "A") {
+            const target = targetElem.getAttribute("href").slice(1);
+            const requestedPageSection = pageSections.find(
+                section => section.id === target
+            );
+
+            removeClass("active-section", pageSections);
+            removeClass("active", navAnchors);
+
+            targetElem.classList.add("active");
+            requestedPageSection.scrollIntoView({ behavior: "smooth" });
+            requestedPageSection.classList.add("active-section");
+
+            setTimeout(() => (
+                clickActive = !clickActive
+            ), 2000);
+        }
+    }
+
+    /**
+     * End Helper Functions
+     * Begin Main Functions
+     *
+    */
+
+    // build the nav
+    const buildNavigationMenu = () => {
+        const navItemsFragment = document.createDocumentFragment();
+
+        pageSections.forEach(section => {
+            const navItem = document.createElement("li");
+            const linkItem = document.createElement("a");
+            const sectionId = section.id;
+
+            linkItem.textContent = section.dataset.nav;
+            linkItem.classList.add("menu__link");
+            linkItem.setAttribute("href", `#${sectionId}`);
+
+            navItem.append(linkItem);
+            navItemsFragment.append(navItem);
+        });
+
+        navItemsContainer.append(navItemsFragment);
+    };
+
+    /**
+     * End Main Functions
+     * Begin Events
+     *
+    */
+
+    const attachEventListeners = () => {
+        navItemsContainer.addEventListener("click", handleClick);
+        global.addEventListener("scroll", handleScroll);
+    };
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+    /**
+     * End Events
+     * Inititalize functions
+     */
+    buildNavigationMenu();
+    attachEventListeners();
+})(window);
