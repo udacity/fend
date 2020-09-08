f104f8c0bbee1456f34962b8fca0f34d1cfb89e7
koby-kramer <koby.e.kramer@gmail.com>
2020-09-08 09:46:22 -0700

    Added server.js routes, app.js funcitons.

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
+
+// Dependencies
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = 8000;
+const server = app.listen(port, () => {
+    console.log('sever started');
+    console.log(`running on localhost: ${port}`);
+});
+
+// Post Route
+const data = [];
+app.post('/add', (req, res) => {
+    newEntry = {
+        temp: req.body.temp,
+        date: req.body.date,
+        feelings: req.body.content,
+    };
+
+    projectData.push(newEntry);
+    console.log(projectData);
+    res.status(204).end()
+});
+
+// Callback function to complete GET '/all'
+app.get('/all', (req, res) => {
+    res.send(projectData);
+});
+
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,53 @@
-/* Global Variables */
+// OpenWeatherMap API
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const API_KEY = '&appid=077ccff7e77db9e6ea5ee2eca04874ef';
 
-// Create a new date instance dynamically with JS
+//Get the date
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
+
+// Get Inputs and call getWeather
+document.getElementById('generate').addEventListener('click', (e) => {
+    const zipCode = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    getWeather(zipCode, feelings);
+});
+
+// getWeather Function
+const getWeather = async (zip, feelings) => {
+    // Fetch the Response
+    const resp = fetch(`${BASE_URL}${zip}${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            return postData('/add', {
+                date: newDate,
+                temp: data.main.temp,
+                content: feelings
+            })
+        })
+        .then(() => fetch('/all'))
+        .then(response => {
+            let responseJSON = response.json();
+            console.log(responseJSON);
+            return responseJSON;
+        })
+        .then(allData => {
+            const data = allData[allData.length - 1];
+            document.getElementById('date').innerHTML = data.date;
+            document.getElementById('temp').innerHTML = data.temp;
+            document.getElementById('content').innerHTML = data.feelings;
+        });
+}
+
+// postData Function
+
+const postData = (url = '', data = {}) => {
+    return fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+}
