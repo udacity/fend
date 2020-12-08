c5c5d1ed543154e40aae2ce87982a891ec611902
d-andretti <73660537+d-andretti@users.noreply.github.com>
2020-12-08 20:42:09 +0000

    complete project

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,28 +18,46 @@
  * 
 */
 
+//identify the list to populate the nav menu by it's id in the html
+const navList = document.getElementById("navbar__list")
 
+//find the list of sections and the total number to be added to the nav list
+const pageSections = document.querySelectorAll("section")
+const noOfSections = pageSections.length
 /**
  * End Global Variables
- * Start Helper Functions
- * 
 */
 
 
 
 /**
- * End Helper Functions
  * Begin Main Functions
- * 
 */
 
 // build the nav
+function buildNav(){
+    //loop through sections to find titles and add them to navList as list items
+    for (let i = 1; i <= noOfSections; i++) {
+        let section = `#section${i}`
+        let sectionContent = document.querySelector(section)
+        let sectionTitle = sectionContent.getAttribute("data-nav")
+        let ListItem = document.createElement('li')
+        ListItem.textContent=(sectionTitle)
+        ListItem.classList.add("menu__link")
+        ListItem.setAttribute("data-nav", section)
+        navList.appendChild(ListItem)
+    }
+}
 
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
+function isOnScreen(e) {
+    const bounding = e.getBoundingClientRect()
+    return (
+        bounding.top >= 0
+        && bounding.bottom <= window.innerHeight
+    )
+}
 
 
 /**
@@ -48,10 +66,30 @@
  * 
 */
 
-// Build menu 
+// Build menu
+buildNav()
 
 // Scroll to section on link click
+navItems = navList.getElementsByTagName("li")
+for (let i=1; i<=navItems.length; i++) {
+    navItems[i-1].addEventListener('click', function (e) {
+        let id = `#section${i}`
+        let section = document.querySelector(id)
+        section.scrollIntoView()
+    });
+}
 
+ 
 // Set sections as active
-
+window.addEventListener("scroll",
+    function addClassActive(){
+        for (let i=0; i < noOfSections; i++) {
+            if (isOnScreen(pageSections[i])){
+                pageSections[i].classList.add("your-active-class");
+            } else { 
+                pageSections[i].classList.remove("your-active-class");
+            }
+        }
+    }
+)
 
