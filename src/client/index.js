5ad00e2f630bbe3959abc0f47b32a6b7fee94e71
Shawn Mak <smakimagine26@gmail.com>
2020-12-27 22:50:28 -0600

    hook up webpack to import scss and css into build

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
