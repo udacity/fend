4cae893ce3f7885c7b25af9d9e095e2ecb617ef2
Shawn Mak <smakimagine26@gmail.com>
2021-01-01 22:20:08 -0600

    update endpoint with new response object for sentiment breakdown

--- src/server/index.js
+++ src/server/index.js
@@ -21,7 +21,7 @@ app.listen(process.env.SERVER_PORT, function () {
   );
 });
 
-//POST route
+// generates sentiment analysis on a valid URL
 app.post("/sentiment-analysis", analyzeURL);
 function analyzeURL(request, response) {
   fetch(
@@ -34,7 +34,51 @@ function analyzeURL(request, response) {
     }
   )
     .then((res) => res.json())
-    .then((analysis) => response.send(analysis))
+    .then((analysis) => {
+      /**
+       * response object:
+       * {
+       *    overall_sentiment: string,
+       *    sentiment_breakdown: {
+       *      "P+": number,
+       *       "P": number,
+       *       "NEU": number,
+       *        ... etc ...
+       *    }
+       * }
+       */
+      const sentiment_response = {
+        overall_sentiment: analysis.score_tag,
+        sentiment_breakdown: {
+          "P+": 0,
+          P: 0,
+          NEU: 0,
+          N: 0,
+          "N+": 0,
+          NONE: 0,
+          "Dog*4%": "milo",
+        },
+      };
+
+      // possible score_tags (sentiments) found in article/blog sentences
+      // P+: strong positive
+      // P: positive
+      // NEU: neutral
+      // N: negative
+      // N+: strong negative
+      // NONE: without sentiment
+      const sentiments = ["P+", "P", "NEU", "N", "N+", "NONE"];
+      sentiments.forEach((sentiment) => {
+        analysis.sentence_list.forEach((sentence) => {
+          if (sentence.score_tag === sentiment) {
+            sentiment_response.sentiment_breakdown[sentiment] =
+              sentiment_response.sentiment_breakdown[sentiment] + 1;
+          }
+        });
+      });
+
+      response.send(sentiment_response);
+    })
     .catch((err) => response.send({ msg: "server error", err }));
 }
 
