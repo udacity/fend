15ab30f4108bc5c0f95d84d8dda0905f5f1b03ee
Isidro Lopez <isidrolopez@Isidros-MacBook-Air.local>
2021-03-20 02:06:20 -0500

    Installed babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    module: {
+            rules: [
+                    {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+                    }
+            ]
+    }
 }
 
