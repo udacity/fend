8aa05358f9e2dee616adbb2e000023fa1a63d875
Isidro Lopez <isidrolopez@Isidros-MacBook-Air.local>
2021-03-20 02:38:46 -0500

    developer and production modes created

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,8 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
-    
+    mode: 'production',
+    devtool: 'source-map',
     module: {
             rules: [
                     {
