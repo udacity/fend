862735544f42ceae57b71655316cb9ff33445129
Isidro Lopez <isidrolopez@Isidros-MacBook-Air.local>
2021-03-20 02:46:53 -0500

    installed clean plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -21,6 +22,15 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
+    new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
     ]
 }
 
