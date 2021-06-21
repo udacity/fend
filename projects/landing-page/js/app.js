e737b21ab1667db809bc9173211a022e7f0eedd8
l-hossam <82220791+l-hossam@users.noreply.github.com>
2021-06-21 03:10:56 +0200

    request ?
    is it  ok  make may code here?

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,3 +1,4 @@
+
 /**
  * 
  * Manipulating the DOM exercise.
@@ -17,7 +18,8 @@
  * Define Global Variables
  * 
 */
-
+const sectionsArr= Array.from(document.querySelectorAll('section'))
+let sectionslength= sectionsArr.length 
 
 /**
  * End Global Variables
@@ -32,14 +34,45 @@
  * Begin Main Functions
  * 
 */
-
-// build the nav
-
-
+function listItemCreator(){
+    //create the items and add the link to it 
+    for (x of sectionsArr){ 
+        let container = document.createElement('li');
+        container.innerHTML= `<a class='menu__link' href= '#${x.getAttribute('id')}'> ${x.getAttribute('data-nav')}</a>`
+    // add the items to the navgation bar that's created by html under navbarlist 
+    document.getElementById('navbar__list').appendChild(container)
+    }
+    }
+// function to see if the section  exist in the visual viewport 
 // Add class 'active' to section when near top of viewport
 
+function portview (el){
+    let y = el.getBoundingClientRect(); 
+    return (y.top>=0);
+}
+// the function to control the apperance.
+function toggleFun(){
+    for (x of sectionsArr){
+        if (portview(x)){
+            if(!x.classList.contains('your-active-class')){
+                x.classList.add('your-active-class'); 
+            }
+        }
+        else {
+                x.classList.remove('your-active-class');
+            }
+        
+    }
+}
 
+
+listItemCreator();
 // Scroll to anchor ID using scrollTO event
+
+document.addEventListener('scroll',toggleFun()); 
+
+
+
 
 
 /**
