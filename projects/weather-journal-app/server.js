6fd613e07365ce6c4ae2cf6f658b47cd1278fcf2
aymandarwish <aymandarwiish@gmail.com>
2021-09-16 06:19:46 +0300

    Home__15-09-21

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -8,14 +8,31 @@ const express = require('express');
 const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, listening);
+// const server = app.listen(port, () => { console.log(`running on localhost: ${port}`) })
+// Callback to debug
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost: ${port}`)
+};
+
+// Create JS object
+const projectData = []
+
+
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -3,3 +3,8 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+let baseURL = 'http://api.animalinfo.org/data/?animal='
+let apiKey = 'nJRv2hgsznJjeVChCrnEO3BauboQpoTqa6GhtIHg';
+const newAnimal =  document.getElementById('animal').value;
