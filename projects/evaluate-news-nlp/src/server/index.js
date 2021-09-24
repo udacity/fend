1c00d4622fe21a4bc424b8bd4d2e7dbb3065d824
Yuli <yuliya@kiwico.com>
2021-09-23 21:49:00 -0700

    fix: Updated the base version to revert to every time I break it

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,25 +1,18 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const bodyParser = require("body-parser");
+var path = require("path");
 
-const application_key = process.env.API_KEY;
+let userEntry = "https://www.kiwico.com"; // for testing
+const apiKey = process.env.API_KEY;
+const apiURL = `https://api.meaningcloud.com/sentiment-2.1?url=${userEntry}&key=${apiKey}&lang=en`;
 
-let userEntry = "https://www.kiwico.com";
-
-const apiPath = `https://api.meaningcloud.com/sentiment-2.1?url=${userEntry}`;
-
-https: var path = require("path");
 const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
-
 const app = express();
-
 app.use(express.static("dist"));
 
-console.log(__dirname);
-
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
 });
 
 // designates what port the app will listen to for incoming requests
@@ -27,6 +20,6 @@ app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
 });
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
-});
+// app.get("/test", function (req, res) {
+//   res.send(mockAPIResponse);
+// });
