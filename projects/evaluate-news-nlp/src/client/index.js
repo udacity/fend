4aa82d70d9ccd55379f6327a50b979c9323b4b12
Sim Neyzi <simneyzi@gmail.com>
2021-12-01 14:18:13 -0800

    prepare working repo

--- projects/evaluate-news-nlp/src/client/index.js
+++
@@ -1,7 +0,0 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -2,18 +2,29 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000
+app.listen(port, listening);
+
+function listening() {
+  console.log('server is up and running on port: ' + port);
+}
