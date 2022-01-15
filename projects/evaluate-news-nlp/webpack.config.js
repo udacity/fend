6bf080deebe4b2a590f952f10c8a0369a8e28cb7
Momotaro-Yvv <84366910+Momotaro-Yvv@users.noreply.github.com>
2022-01-15 13:30:06 +1100

    2nd commit
    add plugin

--- projects/evaluate-news-nlp/webpack.config.js
+++ projects/evaluate-news-nlp/webpack.config.js
@@ -2,9 +2,13 @@
 
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports= {
     //set up the entry point
     entry: './src/client/index.js',
+
+    //Loader: if the Es6 code we write can't be read by client browser, it will automatically translate for us
+    //for all .js files(except node_modules folder), apply babel loader
     module: {
         rules:[
             {
@@ -13,5 +17,14 @@ module.exports= {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+
+    //plugins: we don't need to add reference manually
+    //will create a new index.html file in dist folder
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
