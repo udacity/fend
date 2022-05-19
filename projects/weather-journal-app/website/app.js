6950fcd3ac8c112b6de7396861a4e8c022d9da35
Brightlaz <lazbright1@gmail.com>
2022-05-18 17:30:14 -0700

    Made some required changes
    According to udacity reviews

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,8 +1,7 @@
 // Api url for Open weather map
-const url = 'https://api.openweathermap.org/data/2.5/weather?id=';
-
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 // Personal API Key for OpenWeatherMap API
-const apiKey = '&appid=cb2eeafd3fb2f4ce0b2db2232ed72f9b&units=imperial';
+const apiKey = ',{country code}&appid={API key}&units=imperial';
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
@@ -11,13 +10,25 @@ document.getElementById('generate').addEventListener('click', performAction);
 function performAction(e) {
     const zip = document.getElementById('zip').value;
     const response = document.getElementById('feelings').value;
-    getZip(url, zip, apiKey)
-        .then(function(data) {
-            // Add data
-            postData('/addData', { temperature: data.main.temp, date: data.dt, feel: response })
-        })
-        .then(updateUI())
+    if (zip && validateZipCode(zip)) {
+        getZip(url, zip, apiKey)
+            .then(function(data) {
+                // Add data
+                postData('/addData', { temperature: data.main.temp, date: data.dt, feel: response })
+                    .then(function() {
+                        updateUI()
+                    });
+            })
+    } else {
+        alert('zip code is invalid or not found, input a valid zip code')
+    }
 };
+
+// function to check if zip code is valid
+const validateZipCode = function(elementValue) {
+    var zipCodePattern = /^\d{5}$|^\d{5}-\d{4}$/;
+    return zipCodePattern.test(elementValue);
+}
 
 /* Function to GET Web API Data*/
 const getZip = async(url, zip, key) => {
@@ -56,8 +67,10 @@ const updateUI = async() => {
     const request = await fetch('/all');
     try {
         const allData = await request.json();
+        let d = new Date(allData.date);
+        let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
         document.getElementById('temp').innerHTML = Math.round(allData.temperature) + 'degrees';
-        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('date').innerHTML = newDate
         document.getElementById('content').innerHTML = allData.res;
     } catch (error) {
         console.log("error", error);
