db8908dde971f725a0df38aac767fc3f03476351
Hanni <johanna.liebler@gmail.com>
2022-09-12 10:09:09 +0200

    initial weather app, starting server and api connection

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,37 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
+/* Dependencies */
+const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+const { response } = require("express");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const port = 8080;
+const server = app.listen(port, listening);
+function listening() {
+  console.log(server);
+  console.log(`running on localhost: ${port}`);
+}
+
+const data = [];
+
+//GET route
+app.get("/", (req, res) => {
+  res.send(data);
+});
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,23 @@
 /* Global Variables */
 
+// Personal API Key for OpenWeatherMap API
+
+const baseURL = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={APIKey}`;
+
+const APIKey = process.env.apiKey;
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+/* Function to GET Project Data */
+
 // Create a new date instance dynamically with JS
+
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
